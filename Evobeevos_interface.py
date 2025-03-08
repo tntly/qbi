98cbdbf5dee07d9743a20ce4bc9fe9b3b619f888
michaelwaves <michaelyu713705@gmail.com>
2025-03-08 14:40:20 -0500

    add form form ucsc chromosome

--- Evobeevos_interface.py
+++ Evobeevos_interface.py
@@ -4,7 +4,18 @@ import numpy as np
 
 st.title("EvoBeevos Variant Predictor 🐝")
 
-st.title("Hello this is a title")
+with st.form("uscs-form",clear_on_submit=False, enter_to_submit=True):
+    st.write("Input chromosome address in UCSC format")
+    chromosome_number = st.number_input("Chromosome Number")
+    starting_base_pair = st.text_input("Starting Base Pair")
+    ending_base_pair = st.text_input("Ending Base Pair")
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("Chromosome number",chromosome_number,
+                 "Starting BP", starting_base_pair,
+                 "Ending BP", ending_base_pair
+                 )
+
 
 # Display result and score on main page
 # Display CliVar comparison on main page as well?
