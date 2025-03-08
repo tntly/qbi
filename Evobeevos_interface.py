2af1b2435b1f9e3a7852b9514049fdb2c79be224
michaelwaves <michaelyu713705@gmail.com>
2025-03-08 14:44:47 -0500

    rearrange descritpon and form

--- Evobeevos_interface.py
+++ Evobeevos_interface.py
@@ -4,6 +4,15 @@ import numpy as np
 
 st.title("EvoBeevos Variant Predictor 🐝")
 
+st.header("**About**")
+
+st.write("EvoBeevos is a comprehensive "
+         "Variant Effect Predictor that leverages the Evo 2 AI model "
+         "to predict genetic variant effects. It compares these predictions with "
+         "ClinVar/dbSNP data, offering users a thorough analysis of variant significance. "
+         "The app features a Streamlit interface, API integrations, a lightweight database "
+         "for caching results, and an AI chatbot for user assistance.")
+
 with st.form("uscs-form",clear_on_submit=False, enter_to_submit=True):
     st.write("Input chromosome address in UCSC format")
     chromosome_number = st.number_input("Chromosome Number")
@@ -17,14 +26,6 @@ with st.form("uscs-form",clear_on_submit=False, enter_to_submit=True):
                  )
 
 
-st.header("**About**")
-
-st.write("EvoBeevos is a comprehensive "
-         "Variant Effect Predictor that leverages the Evo 2 AI model "
-         "to predict genetic variant effects. It compares these predictions with "
-         "ClinVar/dbSNP data, offering users a thorough analysis of variant significance. "
-         "The app features a Streamlit interface, API integrations, a lightweight database "
-         "for caching results, and an AI chatbot for user assistance.")
 
 # Display result and score on main page
 # Display CliVar comparison on main page as well?
