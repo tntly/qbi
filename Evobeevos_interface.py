0c5b5b8e60cd8ec4c3942f47536ce59cd356afd7
Heather Ho <heatherho@gmail.com>
2025-03-07 20:25:14 -0800

    Update Evobeevos_interface.py

--- Evobeevos_interface.py
+++ Evobeevos_interface.py
@@ -2,4 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("EvoBeevos Variant Analysis")
+st.title("EvoBeevos Variant Predictor 🐝")
+
+# Display result and score on main page
+# Display CliVar comparison on main page as well?
+# Sidebar will have user input info, or should it also be on main page
+# Where to put chatbot?
+# What kind of input do we want the user to put in (i.e DNA sequences? Should we cap how long the sequence can be?)
